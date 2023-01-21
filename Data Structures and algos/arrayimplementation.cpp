f03c4484ca94141050e2815a851f8832e8efced3
Hackwith-Rohit <rkrai3122@gmail.com>
2023-01-21 21:33:05 +0530

    changes

---
+++ Data Structures and algos/arrayimplementation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int getmin(int num[] , int n)
+{
+    int mini = INT_MAX;
+    for(int i=0 ; i < n ; i++)
+    {
+        mini = min(mini,num[i]);
+    }
+    return mini;
+}
+
+int getmax(int num[], int n) 
+{
+    int maxi = INT_MIN;
+    for(int i = 0; i < n ; i++){
+        maxi = max(maxi,num[i]);
+    }
+    return maxi;
+}
+
+int main()
+{
+    int size;
+    cin>>size;
+
+    int num[100];
+
+    for(int i = 0; i<size ; i++){
+        cin>>num[i];
+    }
+
+    cout << " Maximum value is " << getmax(num, size) << endl;
+    cout << " Minimum value is " << getmin(num, size) << endl;
+
+
+return 0;
+}
+
