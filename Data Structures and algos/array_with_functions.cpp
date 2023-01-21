68972707a64314e5f614986e4972883995835460
Hackwith-Rohit <rkrai3122@gmail.com>
2023-01-21 19:02:18 +0530

    update

---
+++ Data Structures and algos/array_with_functions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    cout <<"Printing the array" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i] <<" ";
+    }
+    cout <<"Printing Done" << endl;
+}
+
+int main()
+{
+    int number[19];
+    cout<<"Value at 14 index " << number[14]<<endl;
+
+    int second [20] = {1 , 2  , 3 , 4 };
+   // int n = 20;
+    printArray(second,20);
+
+}
